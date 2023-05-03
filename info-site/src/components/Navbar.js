1247d9a07b3ea17b87c6870b9b6734303fd716de
Milagros Lasarte <lasartemilagros@gmail.com>
2023-05-03 17:47:05 +0200

    build components

---
+++ info-site/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+
+export default function Navbar() {
+  return (
+    <div>
+      <img src={logo} alt="React Logo" width="40px"/>
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </div>
+  )
+}
