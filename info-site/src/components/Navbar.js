82389f0f02d7997a3cd7459066f1f05e46db42b6
Milagros Lasarte <lasartemilagros@gmail.com>
2023-05-03 18:25:20 +0200

    add styling to components

--- info-site/src/components/Navbar.js
+++ info-site/src/components/Navbar.js
@@ -2,8 +2,8 @@ import logo from '../logo.svg';
 
 export default function Navbar() {
   return (
-    <div>
-      <img src={logo} alt="React Logo" width="40px"/>
+    <div className="navbar">
+      <img src={logo} alt="React Logo" width="50px"/>
       <h3>ReactFacts</h3>
       <h4>React Course - Project 1</h4>
     </div>
