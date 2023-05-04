aca8ffce6be006e7e78cb42f7d19093f73e75edb
Milagros Lasarte <lasartemilagros@gmail.com>
2023-05-04 12:00:46 +0200

    create static components

---
+++ airbnb-experiences/src/components/Hero.js
@@ -0,0 +1,12 @@
+import grid from '../images/photo-grid.png'
+
+export default function Hero() {
+  return(
+    <section className="hero">
+      <img src={grid} alt="Grid" className="hero--photo"/>
+      <h1 className="hero--header">Online experiences</h1>
+      <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.</p>
+    </section>
+  )
+}
