dc200ab5d5f3e677b278b9f64a2b1d2ddb6d9c4b
Milagros Lasarte <lasartemilagros@gmail.com>
2023-05-05 15:38:07 +0200

    refactor states in Meme component

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,13 +1,39 @@
+import { useState } from 'react';
+
 import '../stylesheets/Meme.css'
+import memesData from '../memesData'
+
 
 export default function Meme() {
+  const [meme, setMeme] = useState( {
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+
+
+  function getMemeImage() {
+    const memes = allMemeImages.data.memes
+    const index = Math.floor(Math.random() * memes.length)
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: memes[index].url
+    }))
+  }
+
   return(
-    <form className="input">
+    <div className="input">
       <div className="input--fields">
         <input type="text" id="firstKeyword" placeholder="Top text"/>
         <input type="text" id="secondKeyword" placeholder="Bottom text"/>
       </div>
-      <input type="submit" value="Get a new meme image 🖼" className="input--btn"/>
-    </form>
+      <button className="input--btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
+      <div className="image--center">
+        <img src={meme.randomImage} alt="meme" className="meme"/>
+      </div>
+    </div>
   )
 }
